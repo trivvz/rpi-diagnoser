b438aed7386bda62279a8a72b6048a58bb8dd1fb
Tomasz Wojdat <trivvz3@gmail.com>
2021-01-18 22:29:54 +0100

    Update diagnostic message
    - Show min-max values and calculated average
    - Correct some bugs

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ from my_types import Clock, Temperature, Voltage
 class DiagInfo:
     def __init__(self) -> None:
 
-        self.temp: Temperature = Temperature(0.0)
-        self.min_temp: Temperature = Temperature(0.0)
-        self.max_temp: Temperature = Temperature(0.0)
+        self.temp = self.get_temp()
+        self.temp_min = self.temp
+        self.temp_max = self.temp
         self.temp_list: List[Temperature] = []
 
-        self.voltage: Voltage = Voltage(0.0)
-        self.min_voltage: Voltage = Voltage(0.0)
-        self.max_voltage: Voltage = Voltage(0.0)
+        self.voltage = self.get_voltage()
+        self.voltage_min = self.voltage
+        self.voltage_max = self.voltage
         self.voltage_list: List[Voltage] = []
 
-        self.clock: Clock = Clock(0)
-        self.min_clock: Clock = Clock(0)
-        self.max_clock: Clock = Clock(0)
+        self.clock = self.get_clock()
+        self.clock_min = self.clock
+        self.clock_max = self.clock
         self.clock_list: List[Clock] = []
 
-        self.throttled: str
+        self.throttled = self.get_throttled()
         self.time = datetime.now()
 
     @staticmethod
@@ -55,12 +55,46 @@ class DiagInfo:
         throttled_val = int(utils.call_cmd(GET_THROTTLED), 0)
         return bin(throttled_val)
 
+    def get_temp_avg(self) -> Temperature:
+        return Temperature(sum(self.temp_list) / len(self.temp_list))
+
+    def get_voltage_avg(self) -> Voltage:
+        return Voltage(sum(self.voltage_list) / len(self.voltage_list))
+
+    def get_clock_avg(self) -> Clock:
+        return Clock(sum(self.clock_list) // len(self.clock_list))
+
+    def handle_temp(self) -> None:
+        if self.temp > self.temp_max:
+            self.temp_max = self.temp
+        if self.temp < self.temp_min:
+            self.temp_min = self.temp
+        self.temp_list.append(self.temp)
+
+    def handle_voltage(self) -> None:
+        if self.voltage > self.voltage_max:
+            self.voltage_max = self.voltage
+        if self.voltage < self.voltage_min:
+            self.voltage_min = self.voltage
+        self.voltage_list.append(self.voltage)
+
+    def handle_clock(self) -> None:
+        if self.clock > self.clock_max:
+            self.clock_max = self.clock
+        if self.clock < self.clock_min:
+            self.clock_min = self.clock
+        self.clock_list.append(self.clock)
+
     def update(self) -> None:
         self.time = datetime.now()
         self.temp = self.get_temp()
         self.voltage = self.get_voltage()
         self.clock = self.get_clock()
         self.throttled = self.get_throttled()
+
+        self.handle_temp()
+        self.handle_voltage()
+        self.handle_clock()
 
     def gen_output(self) -> str:
         output = [
@@ -83,11 +117,16 @@ class DiagInfo:
 
 if __name__ == "__main__":
     try:
+        diag_info = DiagInfo()
         while True:
-            diag_info = DiagInfo()
             diag_info.update()
             str(diag_info)
             # gen_log("log.txt")
             time.sleep(2)
     except KeyboardInterrupt:
-        print("\n--- Raspberry Pi diagnostic statistics ---")
+        print(
+            "\n--- Raspberry Pi diagnostic statistics ---"
+            f"\nTemperature min/avg/max = {diag_info.temp_min}/{diag_info.get_temp_avg():.2f}/{diag_info.temp_max}"
+            f"\n    Voltage min/avg/max = {diag_info.voltage_min}/{diag_info.get_voltage_avg():.2f}/{diag_info.voltage_max}"
+            f"\n      Clock min/avg/max = {diag_info.clock_min}/{diag_info.get_clock_avg()}/{diag_info.clock_max}"
+        )
