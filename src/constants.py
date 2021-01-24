11fad81ac8ee1c21f1d9591fa6636aa5fefd85cf
Tomasz Wojdat <trivvz3@gmail.com>
2021-01-24 23:22:03 +0100

    Update throttled constants and add reference

--- src/constants.py
+++ src/constants.py
@@ -12,9 +12,13 @@ MEASURE_CLOCK = "vcgencmd measure_clock arm"
 CLOCK_DIVISOR = 1_000_000
 
 GET_THROTTLED = "vcgencmd get_throttled"
-THROTTLED_VAL_0 = 2  # beginning of first section
-THROTTLED_VAL_3 = 6  # end of first section
-THROTTLED_VAL_16 = -4  # beginning of second section
+
+# Throttled is 20-bit little-endian
+# Reference: https://www.raspberrypi.org/documentation/raspbian/applications/vcgencmd.md
+THROTTLED_BIT_19 = 2  # beginning of first section
+THROTTLED_BIT_16 = 6  # end of first section
+THROTTLED_BIT_3 = -4  # beginning of second section
+
 THROTTLED_SEP = "::"
 
 DEGREE_SIGN = "\N{DEGREE SIGN}"
--- src/throttled.py
+++ src/throttled.py
@@ -1,9 +1,9 @@
 from src import utils
 from src.constants import (
     GET_THROTTLED,
-    THROTTLED_VAL_0,
-    THROTTLED_VAL_3,
-    THROTTLED_VAL_16,
+    THROTTLED_BIT_19,
+    THROTTLED_BIT_16,
+    THROTTLED_BIT_3,
     THROTTLED_SEP,
 )
 
@@ -17,7 +17,7 @@ class Throttled:
         throttled_val = int(utils.call_cmd(GET_THROTTLED), 0)
         throttled_str = f"{throttled_val:#020b}"
         return (
-            f"{throttled_str[THROTTLED_VAL_0:THROTTLED_VAL_3]}"
+            f"{throttled_str[THROTTLED_BIT_19:THROTTLED_BIT_16]}"
             f"{THROTTLED_SEP}"
-            f"{throttled_str[THROTTLED_VAL_16:]}"
+            f"{throttled_str[THROTTLED_BIT_3:]}"
         )
