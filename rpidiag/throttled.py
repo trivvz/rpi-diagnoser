096c30616b63dd80d61d464e366c10531070a3e2
Tomasz Wojdat <trivvz3@gmail.com>
2021-02-21 00:51:48 +0100

    Refactor Throttled class
    - Split get() method into smaller ones

--- rpidiag/throttled.py
+++ rpidiag/throttled.py
@@ -1,23 +1,33 @@
 from rpidiag import utils
 from rpidiag.constants import (
     GET_THROTTLED,
-    THROTTLED_BIT_19,
-    THROTTLED_BIT_16,
     THROTTLED_BIT_3,
+    THROTTLED_BIT_16,
+    THROTTLED_BIT_19,
     THROTTLED_SEP,
 )
 
 
 class Throttled:
-    def __init__(self) -> None:
-        pass
+    def get(self) -> str:
+        throttled_str = self._convert_to_binary()
+        return (
+            self._get_occurred_part(throttled_str)
+            + f"{THROTTLED_SEP}"
+            + self._get_active_part(throttled_str)
+        )
 
     @staticmethod
-    def get() -> str:
-        throttled_val = int(utils.call_cmd(GET_THROTTLED), 0)
-        throttled_str = f"{throttled_val:#020b}"
-        return (
-            f"{throttled_str[THROTTLED_BIT_19:THROTTLED_BIT_16 + 1]}"
-            f"{THROTTLED_SEP}"
-            f"{throttled_str[THROTTLED_BIT_3:]}"
-        )
+    def _get_occurred_part(throttled_bin: str) -> str:
+        return f"{throttled_bin[THROTTLED_BIT_19:THROTTLED_BIT_16 + 1]}"
+
+    @staticmethod
+    def _get_active_part(throttled_bin: str) -> str:
+        return f"{throttled_bin[THROTTLED_BIT_3:]}"
+
+    def _convert_to_binary(self) -> str:
+        return f"{self._get_raw_value():#020b}"
+
+    @staticmethod
+    def _get_raw_value() -> int:
+        return int(utils.call_cmd(GET_THROTTLED), 0)
