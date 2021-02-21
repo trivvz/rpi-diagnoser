6e31b4abff6c72060e01793919f47762083de839
Tomasz Wojdat <trivvz3@gmail.com>
2021-02-21 01:37:21 +0100

    Add test of _get_binary and _get_raw_value

--- tests/test_throttled.py
+++ tests/test_throttled.py
@@ -11,3 +11,16 @@ def test_get_active_part():
     raw_output = "0b101100000000001001"
     throttled = Throttled()
     assert throttled._get_active_part(raw_output) == "1001"
+
+
+def test_get_binary(mocker):
+    raw_output = "0b000000000000000000"
+    mocker.patch("rpidiag.utils.call_cmd", return_value="0x0")
+    throttled = Throttled()
+    assert throttled._get_binary() == raw_output
+
+
+def test_get_raw_value(mocker):
+    mocker.patch("rpidiag.utils.call_cmd", return_value="0x0")
+    throttled = Throttled()
+    assert throttled._get_raw_value() == 0
