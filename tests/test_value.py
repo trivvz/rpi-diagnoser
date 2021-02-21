4252375b6dd6c63357f2f4ab1e1daece7ed17bb9
Tomasz Wojdat <trivvz3@gmail.com>
2021-02-21 12:18:30 +0100

    Create one combined test for Value.update()

--- tests/test_value.py
+++ tests/test_value.py
@@ -17,29 +17,28 @@ def test_value_get_avg(test_input, expected):
     assert pytest.approx(value.get_avg(), abs=0.001) == expected
 
 
-def test_value_update_max(mocker):
-    mocker.patch("rpidiag.value.Temperature.get", return_value=20)
+@pytest.mark.parametrize(
+    "initial_min, initial_max, test_input, expected_min, expected_max",
+    [
+        (15, 15, 20, 15, 20),
+        (15, 15, 10, 10, 15),
+        (15, 15, 15, 15, 15),
+        (10, 20, 15, 10, 20),
+        (10, 20, 30, 10, 30),
+        (10, 20, 5, 5, 20),
+    ],
+)
+def test_value_update(
+    initial_min, initial_max, test_input, expected_min, expected_max, mocker
+):
+    mocker.patch("rpidiag.value.Temperature.get", return_value=test_input)
     value = Value(Temperature.get)
-    value.max = 15
-    value.min = 15
+    value.min = initial_min
+    value.max = initial_max
     value.update()
 
-    assert value.min == 15
-    assert value._get_summary()["min"] == 15
+    assert value.min == expected_min
+    assert value._get_summary()["min"] == expected_min
 
-    assert value.max == 20
-    assert value._get_summary()["max"] == 20
-
-
-def test_value_update_min(mocker):
-    mocker.patch("rpidiag.value.Temperature.get", return_value=10)
-    value = Value(Temperature.get)
-    value.max = 15
-    value.min = 15
-    value.update()
-
-    assert value.min == 10
-    assert value._get_summary()["min"] == 10
-
-    assert value.max == 15
-    assert value._get_summary()["max"] == 15
+    assert value.max == expected_max
+    assert value._get_summary()["max"] == expected_max
