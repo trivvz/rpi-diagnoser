9bd8cc33112842d085d4defa1d2d8457fb4dda39
Tomasz Wojdat <trivvz3@gmail.com>
2021-02-21 11:53:27 +0100

    Use mocker in test_value.py

--- tests/test_value.py
+++ tests/test_value.py
@@ -1,20 +1,15 @@
-from rpidiag.value import Value
+from rpidiag.value import Value, Temperature
 
 
 def test_value_get_avg():
-    def getter():
-        return 0
-
-    value = Value(getter)
+    value = Value(Temperature.get)  # any getter is fine
     value.all = [10, 20]
     assert value.get_avg() == 15
 
 
-def test_value_update_max():
-    def getter():
-        return 20
-
-    value = Value(getter)
+def test_value_update_max(mocker):
+    mocker.patch("rpidiag.value.Temperature.get", return_value=20)
+    value = Value(Temperature.get)
     value.max = 15
     value.min = 15
     value.update()
@@ -26,11 +21,9 @@ def test_value_update_max():
     assert value._get_summary()["max"] == 20
 
 
-def test_value_update_min():
-    def getter():
-        return 10
-
-    value = Value(getter)
+def test_value_update_min(mocker):
+    mocker.patch("rpidiag.value.Temperature.get", return_value=10)
+    value = Value(Temperature.get)
     value.max = 15
     value.min = 15
     value.update()
