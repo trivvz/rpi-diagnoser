dc36ea514b7dc6f314dcfea7b78d5fddad1b62b8
Tomasz Wojdat <trivvz3@gmail.com>
2021-05-16 21:32:30 +0200

    Add basic CLI with argparse

--- rpidiag/main.py
+++ rpidiag/main.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 from rpidiag import diag_info
@@ -6,17 +7,26 @@ from rpidiag import output_handler as oh
 
 
 def cli() -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-q", "--quiet", action="store_true", help="quiet mode")
+
+    args = parser.parse_args()
 
     if IS_LOGGING_ENABLED:
         oh.check_save_permissions()
 
-    oh.print_header()
+    if not args.quiet:
+        oh.print_header()
+
     diag = diag_info.DiagInfo()
 
     try:
         while True:
             diag.update()
-            print(diag.get_output())
+
+            if not args.quiet:
+                print(diag.get_output())
 
             if IS_LOGGING_ENABLED:
                 diag.log()
@@ -24,4 +34,7 @@ def cli() -> None:
             time.sleep(REFRESH_TIME)
 
     except KeyboardInterrupt:
-        print(diag.get_summary())
+        if not args.quiet:
+            print(diag.get_summary())
+        else:  # after ^C prompt should go to the next line
+            print()
