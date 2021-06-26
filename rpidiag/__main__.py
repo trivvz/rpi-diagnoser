6792bcf87ee36bb1e5c8083e5246705a915f22d0
Tomasz Wojdat <trivvz3@gmail.com>
2021-06-26 15:27:45 +0200

    Rename cli() to main()
    This name better follows the convention

--- rpidiag/__main__.py
+++ rpidiag/__main__.py
@@ -4,7 +4,7 @@ import sys
 if not __package__:
     sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from rpidiag.main import cli
+from rpidiag.main import main
 
 if __name__ == "__main__":
-    sys.exit(cli())
+    sys.exit(main())
